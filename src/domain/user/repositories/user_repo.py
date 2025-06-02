d78d2e814a0f94b311b3142307960f34a221ef35
Andrey <snick25rs@gmail.com>
2025-06-02 19:10:16 +0200

    Исправил по PEP

--- src/domain/user/repositories/user_repo.py
+++ src/domain/user/repositories/user_repo.py
@@ -7,44 +7,44 @@ class UserRepository(ABC):
     @abstractmethod
     def save(self, user: User) -> Optional[User]:
         """Добавляет юзера"""
-        pass
+        ...
 
     @abstractmethod
     def get_by_id(self, user_id: UUID) -> Optional[User]:
         """Возвращает пользователя по его id"""
-        pass
+        ...
 
     @abstractmethod
     def get_by_username(self, username: str) -> Optional[User]:
         """Возвращает пользователя по его id"""
-        pass
+        ...
 
     @abstractmethod
     def exists_by_email(self, email: str) -> Optional[bool]:
         """Проверяет существование пользователя с указанным email"""
-        pass
+        ...
 
     @abstractmethod
     def exists_by_username(self, username: str) -> Optional[bool]:
         """Проверяет существование пользователя с указанным username"""
-        pass
+        ...
 
     @abstractmethod
     def change_password(self, user_id: UUID, password: str) -> Optional[User]:
         """Меняет пароль у пользователя"""
-        pass
+        ...
 
     @abstractmethod
     def change_username(self, user_id: UUID, username: str) -> Optional[User]:
         """Меняет username у пользователя"""
-        pass
+        ...
 
     @abstractmethod
     def change_name(self, user_id: UUID, name: str) -> Optional[User]:
         """Меняет username у пользователя"""
-        pass
+        ...
 
     @abstractmethod
     def change_email(self, user_id: UUID, email: str) -> Optional[User]:
         """Меняет username у пользователя"""
-        pass
+        ...
