e1f1f3df5a7f8eb40675cf6351dc0577331e6f4f
Andrey <snick25rs@gmail.com>
2025-06-02 19:09:29 +0200

    Исправил по PEP

--- src/domain/link/repositories/link_repo.py
+++ src/domain/link/repositories/link_repo.py
@@ -3,18 +3,19 @@ from src.domain.link.models.link import Link
 from typing import Optional
 from uuid import UUID
 
+
 class LinkRepository(ABC):
 	@abstractmethod
 	def create(self, link: Link) -> Link:
 		"""Добавляет линк"""
-		pass
+		...
 
 	@abstractmethod
 	def get_by_id(self, link_id: int, user_id: UUID = None) -> Optional[Link]:
 		"""Возвращает линк по id"""
-		pass
+		...
 
 	@abstractmethod
 	def get_by_alias(self, alias: str, user_id: UUID = None) -> Optional[Link]:
 		"""Возвращает линк по alias"""
-		pass
+		...
