106b3f185316f75dd77d87c4ffb59b509a29e99c
Andrey <snick25rs@gmail.com>
2025-06-17 22:46:13 +0200

    Улучшил читаемость

--- src/infra/repositories/exceptions/link_exception.py
+++ src/infra/repositories/exceptions/link_exception.py
@@ -1,21 +1,18 @@
 from src.domain.link.exceptions.link_exceptions import (
-InvalidCreateLink,
-InvalidCheckShortCode,
-InvalidGetLink,
-InvalidGetAllLinks,
-InvalidDeleteLink,
-InvalidDeleteAllLinks,
-LinkNotExists,
-LinksNotExist,
+	InvalidCreateLink,
+	InvalidCheckShortCode,
+	InvalidGetLink,
+	InvalidGetAllLinks,
+	InvalidDeleteLink,
+	InvalidDeleteAllLinks,
+	LinkNotExists,
+	LinksNotExist,
 )
-
-#TODO: Добавить tab и убрать комменты
 
 LAYER = "Infra/repositories/link"
 
 
 class InfraInvalidCreateLink(InvalidCreateLink):
-	"""Invalid Create Link"""
 	message = "Invalid Create Link"
 
 	def __init__(self):
@@ -23,7 +20,6 @@ class InfraInvalidCreateLink(InvalidCreateLink):
 
 
 class InfraInvalidCheckShortCode(InvalidCheckShortCode):
-	"""Invalid Check Short Code"""
 	message = "Invalid Check Short Code"
 
 	def __init__(self):
@@ -31,7 +27,6 @@ class InfraInvalidCheckShortCode(InvalidCheckShortCode):
 
 
 class InfraInvalidGetLink(InvalidGetLink):#get_by_alias
-	"""Invalid Get Link"""
 	message = "Invalid Get Link"
 
 	def __init__(self):
@@ -39,7 +34,6 @@ class InfraInvalidGetLink(InvalidGetLink):#get_by_alias
 
 
 class InfraInvalidGetAllLinks(InvalidGetAllLinks):
-	"""Invalid Get All Links"""
 	message = "Invalid Get All Links"
 
 	def __init__(self):
@@ -47,7 +41,6 @@ class InfraInvalidGetAllLinks(InvalidGetAllLinks):
 
 
 class InfraInvalidDeleteLink(InvalidDeleteLink):
-	"""Invalid Delete Link"""
 	message = "Invalid Delete Link"
 
 	def __init__(self):
@@ -55,20 +48,17 @@ class InfraInvalidDeleteLink(InvalidDeleteLink):
 
 
 class InfraInvalidDeleteAllLinks(InvalidDeleteAllLinks):
-	"""Invalid Delete All Links"""
 	message = "Invalid Delete All Links"
 
 	def __init__(self):
 		super().__init__(layer=LAYER, message=self.message)
 
 class InfraLinkNotExists(LinkNotExists):
-	"""Link Doesn't Exist"""
 	message = "Link Doesn't Exist"
 	def __init__(self):
 		super().__init__(layer=LAYER, message=self.message)
 
 class InfraLinksNotExist(LinksNotExist):
-	"""Links Don't Exist"""
 	message = "Links Don't Exist"
 	def __init__(self):
 		super().__init__(layer=LAYER, message=self.message)
