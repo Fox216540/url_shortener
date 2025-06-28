8c25d007bf9dff09b0a254a59b809ef65538bf49
Andrey <snick25rs@gmail.com>
2025-06-28 20:03:16 +0200

    Изменил обработку исключений для возврата статуса 400 при ошибках LinkNotFound, UserNotFound, MessageNotFound, TokenStorageNotFound и UserDataException

--- src/api/dtos/exceptions/error.py
+++ src/api/dtos/exceptions/error.py
@@ -1,16 +1,21 @@
 from fastapi import HTTPException
-from starlette.status import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
+from starlette.status import HTTP_400_BAD_REQUEST, HTTP_500_INTERNAL_SERVER_ERROR
 from src.domain.link.exceptions.link_exceptions import LinkNotFoundException
 from src.domain.user.exceptions.user_exceptions import UserNotFoundException
 from src.domain.message.exceptions.message_exceptions import MessageNotFoundException
 from src.domain.security.exceptions.token_storage_exception import TokenStorageNotFoundException
+from src.app.exceptions.user_exceptions import UserDataException
 from src.api.dtos.exceptions.error_messages import BadRequestErrorMessage, InternalServerErrorMessage
 from src.logger import status_logger
 
 class Error:
     def handle(self, exc: Exception) -> HTTPException:
         status_logger.info(f"Error occurred: \n{exc}")
-        if isinstance(exc, (LinkNotFoundException, UserNotFoundException, MessageNotFoundException, TokenStorageNotFoundException)):
-            raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=BadRequestErrorMessage)
+        if isinstance(exc, (
+		        LinkNotFoundException, UserNotFoundException,
+		        MessageNotFoundException, TokenStorageNotFoundException,
+	            UserDataException
+        )):
+            raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=BadRequestErrorMessage)
         raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR, detail=InternalServerErrorMessage)
 
