c5aa507f0f9b6f0170403f95a3cd3c7931645c1c
Andrey <snick25rs@gmail.com>
2025-07-01 23:53:47 +0200

    Добавил новый ответ с чатом для получения оригинальной ссылки и обновил обработку ссылок

--- src/api/dtos/link_dto.py
+++ src/api/dtos/link_dto.py
@@ -1,13 +1,20 @@
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, HttpUrl, WebsocketUrl
+from uuid import UUID
 
 
 class CreateLinkRequest(BaseModel):
-    url_origin: HttpUrl
+	url_origin: HttpUrl
 
 
 class CreateLinkResponse(BaseModel):
-    url_short: str
+	url_short: str
 
 
 class GetUrlOriginResponse(BaseModel):
-    url_origin: HttpUrl
+	url_origin: HttpUrl
+	
+class GetUrlOriginWithChatResponse(BaseModel):
+	url_short: HttpUrl
+	url_origin: HttpUrl
+	ws: WebsocketUrl
+	room_id: UUID
--- src/api/link_handlers.py
+++ src/api/link_handlers.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Request, Depends
-from fastapi.responses import RedirectResponse
-from src.api.dtos.link_dto import (CreateLinkResponse,CreateLinkRequest)
+from pydantic import HttpUrl, WebsocketUrl
+from src.api.dtos.link_dto import (
+	CreateLinkResponse, CreateLinkRequest,
+	GetUrlOriginResponse, GetUrlOriginWithChatResponse
+)
 from src.api.di.di import get_link_service, get_user_service, get_error
 from settings import URL
 from src.app.service.link_service import LinkService
@@ -9,8 +12,7 @@ from src.api.exceptions.error import Error
 
 router = APIRouter(tags=["link"])
 
-#TODO: Добавить запись ws и original_url и room_id по short_code в куки и редирект на html файл чата: endpoint /link/short_code/c
-@router.get("/{short_code}")
+@router.get("/{short_code}", response_model=GetUrlOriginResponse)
 def get_original_link(request: Request,
                       short_code: str,
                       link_service: LinkService = Depends(get_link_service),
@@ -25,9 +27,37 @@ def get_original_link(request: Request,
 			if user and user.id:
 				user_id = user.id
 		url = link_service.get_url_by_short_code_or_alias(short_code, user_id).original_url
-		return RedirectResponse(str(url))
+		return GetUrlOriginResponse(
+			url_origin=url,
+		)
 	except Exception as e:
 		return error.handle(e)
+
+@router.get("/{short_code}/c", response_model=GetUrlOriginWithChatResponse)
+def get_original_link_with_chat(
+		request: Request,
+		short_code: str,
+		link_service: LinkService = Depends(get_link_service),
+		user_service: UserService = Depends(get_user_service),
+        error: Error = Depends(get_error)
+):
+	try:
+		hostname = request.url.hostname
+		candidate = hostname.split(".")[0]
+		user = user_service.get_user_by_username(candidate)
+		user_id = user.id
+		username = user.username
+		room_id = link_service.get_url_by_short_code_or_alias(short_code, user_id).room_id
+		original_url = link_service.get_url_by_short_code_or_alias(short_code, user_id).original_url
+		ws = WebsocketUrl(f"ws://{URL}/ws/{room_id}")
+		return GetUrlOriginWithChatResponse(
+			url_origin=original_url,
+			ws=ws,
+			url_short=HttpUrl(f"http://{username}.{URL}/{short_code}"),
+			room_id=room_id
+		)
+	except Exception as e:
+		error.handle(e)
 
 @router.post("/short", response_model=CreateLinkResponse)
 def create_short_link(request: CreateLinkRequest,
